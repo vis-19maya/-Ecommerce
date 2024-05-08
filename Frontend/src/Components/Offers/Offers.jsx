da80d67effdd65da864b33f603b82c7cfd34f4af
vis-19maya <165902604+vis-19maya@users.noreply.github.com>
2024-05-08 21:46:39 +0530

    Add files via upload

---
+++ Frontend/src/Components/Offers/Offers.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Offers.css';
+import exclusive_image from '../Assets/offer.jpg';
+const Offers = () => {
+    return(
+        <div className="offers">
+      
+        <div className="offers-right">
+             <img src={exclusive_image} alt=""/>
+        </div>
+    
+        </div>
+    )
+}
+export default Offers;
