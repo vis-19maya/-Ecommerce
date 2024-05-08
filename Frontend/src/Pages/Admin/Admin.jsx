3cf45b8111abec681536d7fa7c4edce0910ca352
vis-19maya <165902604+vis-19maya@users.noreply.github.com>
2024-05-08 21:26:23 +0530

    Add files via upload

---
+++ Frontend/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,14 @@
+
+import './Admin.css'
+import Sidebar from '../../../../frontend/src/Components/Sidebar/Sidebar'
+const Admin = () => {
+  return (
+    <div className='admin'>
+     <Sidebar/>  
+       
+      
+        </div>
+  )
+}
+
+export default Admin
